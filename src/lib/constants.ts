5053acce7b77238ec47ae71946643357119cf78f
Criado Sébastien <dev@letgy.com>
2023-11-05 14:08:31 +0100

    initial commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,4 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  github: "https://github.com/sebcriado",
+  linkedin: "https://www.linkedin.com/in/s%C3%A9bastien-criado-19427a212/",
 };
